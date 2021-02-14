2735821067622c2c4f167908caaf005b40d4e4eb
Peter Butkovic <butkovic@gmail.com>
2021-02-14 10:44:17 +0100

    refactor: shorter syntax

--- src/api/song.js
+++ src/api/song.js
@@ -40,13 +40,56 @@ const _songs = [
     artist: "John Newton",
     key: "D",
     text: [
-      "{start_of_verse: Verse 1}",
+      "{sov: Verse 1}",
       "A[D]mazing grace! (how [G]sweet the [D]sound)",
       "That [D]saved a wretch like [A7]me!",
       "I [D]once was lost, but [G]now am [D]found,",
       "Was [D]blind, but [A7]now I [D]see.",
     ],
   },
+//   {
+//     id: "4",
+//     title: "Holubí dům",
+//     text: [
+//       "{sov: Verse 1}",
+// "[e]Zpí[D]vám [C]ptákům a [h]zvlášť holubům[e]",
+// "stá[D]val v [C]údolí [h]mém starý [e]dům",
+// "[G]ptá[D]ků [G]houf zalétal[D] ke krovům[G]",
+// "[e]měl [D]jsem [C]rád holubích[h] křídel šum[e]",
+// "{eov}",
+// "{sov: Verse 2}",
+// "[e]Vlí[D]dná [C]dívka jim [h]házela hrach[e]",
+// "mává[D]ní [C]perutí [h]víří [e]prach",
+// "[G]ptá[D]cí [G]krouží a neznají strach
+// "[e]měl [D]jsem [C]starý dům [h] jeho práh[e]",
+// "{eov}",
+// "{soc}",
+// "Hledám[C]dům holubí[D]",
+// "kdopak[G]z vás cestu [e]ví",
+// "míval [a]stáj roube[D]nou bílý štít[G]",
+
+// kde je [C]dům holubí [D]a ta [G]dívka kde [Emi]spí
+
+// vždyť to [Ami]ví že jsem [Hmi]chtěl pro ni [Emi]žít
+
+
+
+// Sdílný déšť vypráví okapům, bláhový kdo
+
+// hledá tenhle dům, odrůstáš chlapeckým
+
+// střevícům, neslyšíš holubích křídel šum
+
+
+
+// Nabízej úplatou cokoli nepojíš cukrových
+
+// homolí, můžeš mít třeba zrak sokolí
+
+// nespatříš ztracené údolí
+
+//     ]
+//   }
 ];
 
 export default {
