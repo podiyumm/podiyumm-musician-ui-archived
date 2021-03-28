accf57d6ece5f5878b2a72d68d88e8ca5da1a00a
Peter Butkovic <butkovic@gmail.com>
2021-03-28 10:03:24 +0200

    chore: formatting

--- src/store/modules/song.js
+++ src/store/modules/song.js
@@ -1,99 +1,93 @@
-import SongApiService from '@/api/song'
-import router from '@/router'
+import SongApiService from "@/api/song";
+import router from "@/router";
 
-import {
-  FETCH,
-  FETCH_ALL
-} from "@/store/actions.type";
+import { FETCH, FETCH_ALL } from "@/store/actions.type";
 
 import {
   ID,
   ACTIVE_CHORDS,
   ACTIVE_SONG,
-  ACTIVE_TEXT
-} from '@/store/getters.type';
+  ACTIVE_TEXT,
+} from "@/store/getters.type";
 
-import {
-  SET_ACTIVE,
-  SET_SONGS
-} from '@/store/mutations.type';
+import { SET_ACTIVE, SET_SONGS } from "@/store/mutations.type";
 
 // inspiration: https://github.com/vuejs/vuex/blob/dev/examples/shopping-cart/store/modules/cart.js
 
 // initial state
 const state = () => ({
-    songs: [],
-    active: {},
-  })
+  songs: [],
+  active: {},
+});
 
-  // getters
-  const getters = {
-    [ID]() {
-      return router.currentRoute.params.id
-    },
+// getters
+const getters = {
+  [ID]() {
+    return router.currentRoute.params.id;
+  },
 
-    [ACTIVE_SONG](state) {
-        return state.active;
-    },
+  [ACTIVE_SONG](state) {
+    return state.active;
+  },
 
-    [ACTIVE_TEXT](state, getters) {
-    if (!getters.activeSong.text) return ""
-      return getters.activeSong.text.join("\n");
-    },
+  [ACTIVE_TEXT](state, getters) {
+    if (!getters.activeSong.text) return "";
+    return getters.activeSong.text.join("\n");
+  },
 
-    [ACTIVE_CHORDS](state, getters) {
-      if (!getters.activeText) return ""
-      const chords =  getters.activeText.match(/\[(.*?)\]/g).map(function (elem) {
-        elem = elem.replace(/[[\]]/g,'');
+  [ACTIVE_CHORDS](state, getters) {
+    if (!getters.activeText) return "";
+    const chords = getters.activeText
+      .match(/\[(.*?)\]/g)
+      .map(function(elem) {
+        elem = elem.replace(/[[\]]/g, "");
         return elem;
       })
       // filter empty elems,
       // see: https://stackoverflow.com/questions/55685037/how-to-remove-empty-array-values-from-an-array/55685110
-      .filter(item => item);
+      .filter((item) => item);
 
-      return [...new Set(chords)]
-    },
+    return [...new Set(chords)];
+  },
+};
 
-  }
+// actions
+const actions = {
+  [FETCH_ALL]({ commit }) {
+    return SongApiService.query()
+      .then(({ data }) => {
+        commit(SET_SONGS, data);
+      })
+      .catch((error) => {
+        throw new Error(error);
+      });
+  },
 
-  // actions
-  const actions = {
+  [FETCH]({ commit, getters }) {
+    return SongApiService.get(getters.id)
+      .then(({ data }) => {
+        commit(SET_ACTIVE, data);
+      })
+      .catch((error) => {
+        throw new Error(error);
+      });
+  },
+};
 
-    [FETCH_ALL]({ commit }) {
-      return SongApiService.query()
-        .then(({ data }) => {
-          commit(SET_SONGS, data);
-        })
-        .catch(error => {
-          throw new Error(error);
-        });
-    },
+// mutations
+const mutations = {
+  [SET_SONGS](state, songs) {
+    state.songs = songs;
+  },
+  [SET_ACTIVE](state, song) {
+    state.active = song;
+  },
+};
 
-    [FETCH]({ commit, getters }) {
-      return SongApiService.get(getters.id)
-        .then(({ data }) => {
-          commit(SET_ACTIVE, data);
-        })
-        .catch(error => {
-          throw new Error(error);
-        });
-    }
-  }
-
-  // mutations
-  const mutations = {
-    [SET_SONGS](state, songs) {
-      state.songs = songs;
-    },
-    [SET_ACTIVE](state, song) {
-      state.active = song
-    }
-  }
-
-  export default {
-    namespaced: true,
-    state,
-    getters,
-    actions,
-    mutations
-  }
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations,
+};
